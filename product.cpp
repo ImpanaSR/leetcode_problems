2de8b5dbec0605da3d3cdc5bc21b17377458c2a8
ImpanaSR <impanasr.krn@gmail.com>
2025-07-24 22:53:45 +0530

    add changes

--- product.cpp
+++ product.cpp
@@ -23,6 +23,39 @@ vector<int> productExceptSelf(vector<int>& nums) {
     return ans;
 }
 
+// vector<int> productExceptSelf(vector<int>& nums){
+//     int n = nums.size();
+//     vector<int> ans(n,1);
+
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             if(i!=j) {
+//                 ans[i]*=nums[j];
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+
+//optimised code
+
+// vector<int> productExceptSelf(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> ans(n, 1);
+
+//     for(int i=1; i<n; i++){
+//         ans[i]=ans[i-1]*nums[i-1];
+//     }
+
+//     int suffix=1;
+//     for(int i=n-2; i>=0; i--){
+//         suffix*=nums[i+1];
+//         ans[i]*=suffix;
+//     }
+//     return ans;
+// }
+
 int main() {
     vector<int> nums = {1, 2, 3, 4};
     vector<int> result = productExceptSelf(nums);
